a76f19a55abd3c8a528dbe1cb02db9bcf5dc77e3
Alice Yu <75595719+impedaka@users.noreply.github.com>
2022-04-05 13:44:16 -0400

    Update 2.js

--- pages/blogs/2.js
+++ pages/blogs/2.js
@@ -6,7 +6,7 @@ const blog2 = () => {
   return (
   <Container maxW="container.md" pt={14}>
     <Break/>
-    <VStack align="start">
+    <VStack align="start" spacing={3}>
       <Heading>
         Air Pollution
       </Heading>
@@ -15,20 +15,20 @@ const blog2 = () => {
 Air pollution affects our health, the environment, building structure and the economy. Natural sources of air pollution include forest fires, volcanoes, and emissions of volatile organic compounds from vegetation. However, human activities cause more air pollution. Approximately 65% of the air pollution is caused by humans such as transportation, off-road vehicles and mobile activities, electrical utilities, industrial processes such as oil and gas production. Air pollution kills an estimated seven million people worldwide every year. The World Health Organization shows that most of the global population (99%) breathe air that has elevated levels of pollutants. Ambient air pollution causes an estimate of 4.2 million deaths per year due to strokes, heart diseases, lung cancer and acute and chronic respiratory diseases. 
       
       </Text>
-      <br/>
+
            <Heading size="md">How To Make A Change</Heading>
       <Text>
 
 We can still reduce pollution in the air by using public transportation, bikes, or walking whenever possible. Driving your car less or whenever necessary. Planting trees and plants is a great way to care for the Earth. Use the recycle, reduce and reuse method before throwing out trash. Turning off lights when not in use. These are small actions we can take to reduce pollution and keep our country and earth a clean place.  
       </Text>
-      <br/>
+
       <Text>
         This phenomenon harshly impacts Canada, as our northern country&apos;s permafrost has been thawing at unprecedented rates. This permafrost makes up much of our northern territories, and it&apos;s melting takes away habitat for arctic animals such as snow owls. These animals have increasingly been found interacting with humans, as they are forced to move southward to find habitable land. Not only are land animals impacted, all oceanic lifeforms are harmed by this problem. A problem we caused.
       </Text>
-      <Break/>
+      
         <Image borderRadius="lg" w="full" src={"https://media.discordapp.net/attachments/634834381927743498/959513192080506930/IMG_9157.webp"} alt={"a"} mb={4} />
       
-          <Break/>
+         
         <Image borderRadius="lg" w="full" src={"https://media.discordapp.net/attachments/634834381927743498/959513241216757780/IMG_9158.jpg?width=719&height=479"} alt={"a"} mb={4} />
       <Heading size="md">Sources</Heading>
 
