1d99aa03038f6e2a1430dcafb2542100a2bdf13c
Alice Yu <75595719+impedaka@users.noreply.github.com>
2022-04-05 13:38:39 -0400

    Update 4.js

--- pages/blogs/4.js
+++ pages/blogs/4.js
@@ -7,7 +7,7 @@ const blog4 = () => {
   return (
   <Container maxW="container.md" pt={14}>
     <Break/>
-    <VStack align="start">
+    <VStack align="start" gap={3}>
       <Heading>
         Melting Ice Caps: A Global Emergency
       </Heading>
@@ -16,20 +16,19 @@ const blog4 = () => {
         As we are all aware, global temperatures are increasing at an alarming rate. This increase has lead to more ice melting, year after year. While some ice cap melting during the warmer months of the year is part of Earth&apos;s natural processes, the record losses of ice over the past 10 years is more than concerning.
       
       </Text>
-      <br/>
+
           <Image borderRadius="lg" w="full" src={"https://media.discordapp.net/attachments/634834381927743498/959513241216757780/IMG_9158.jpg?width=719&height=479"} alt={"a"} mb={4} />
       <Text>
 
 The melting of glaciers in the poles lead to higher sea levels, and this increase in liquid water on our Earth is thoroughly detrimental. It causes temperatures to rise even further, and can cause flooding when the land level is low enough. If this pattern continues into the far future, much of our current lands will be submerged in water.
       </Text>
-      <br/>
+
       <Text>
         This phenomenon harshly impacts Canada, as our northern country&apos;s permafrost has been thawing at unprecedented rates. This permafrost makes up much of our northern territories, and it&apos;s melting takes away habitat for arctic animals such as snow owls. These animals have increasingly been found interacting with humans, as they are forced to move southward to find habitable land. Not only are land animals impacted, all oceanic lifeforms are harmed by this problem. A problem we caused.
       </Text>
       <Break/>
         <Image borderRadius="lg" w="full" src={"https://media.discordapp.net/attachments/634834381927743498/959513192080506930/IMG_9157.webp"} alt={"a"} mb={4} />
-      
-          <Break/>
+   
         
       <Heading size="md">Sources</Heading>
 
